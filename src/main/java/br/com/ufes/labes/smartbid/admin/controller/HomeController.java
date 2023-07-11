84534f1099179458d0f4df2f22e9ba193b891c2d
Igor Sunderhus e Silva <igorsilvaspecial@gmail.com>
2023-07-11 08:52:26 -0300

    Exemplificação de consulta com Jena

--- src/main/java/br/com/ufes/labes/smartbid/admin/controller/HomeController.java
+++ src/main/java/br/com/ufes/labes/smartbid/admin/controller/HomeController.java
@@ -4,11 +4,13 @@ import jakarta.faces.view.ViewScoped;
 import jakarta.inject.Named;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.jena.query.QueryExecution;
-import java.text.MessageFormat;
+import java.util.logging.Logger;
 
 @Named
 @ViewScoped
 public class HomeController implements java.io.Serializable {
+    private static final Logger log = Logger.getLogger(HomeController.class.getName());
+
     private String desc;
 
     private String city;
@@ -30,21 +32,32 @@ public class HomeController implements java.io.Serializable {
     }
 
     public void search() {
+
+        log.info("searching for " + this.city);
         if (!StringUtils.isBlank(this.city) && this.city.length() > 3) {
 
-            final String query = MessageFormat.format(
+            this.desc = null;
+            final String query =
                     " PREFIX dbo: <http://dbpedia.org/ontology/> "
                             + " PREFIX dbp: <http://dbpedia.org/property/> "
                             + " PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> "
                             + " SELECT ?desc "
                             + " WHERE { "
                             + "    ?uri a dbo:City; "
-                            + "        dbp:name '{0}'@en; "
-                            + "        rdfs:comment ?desc }",
-                    this.city);
+                            + "        dbp:name '" + this.city + "'@en; "
+                            + "        rdfs:comment ?desc. "
+                            + " FILTER (lang(?desc) = 'en') "
+                            + "}";
+
+            log.info("query: " + query);
 
             try (final QueryExecution qe = QueryExecution.service("https://dbpedia.org/sparql", query)) {
-                qe.execSelect().forEachRemaining(qs -> this.desc = qs.getLiteral("desc").getString());
+                qe.execSelect().forEachRemaining(qs -> {
+                    if (this.desc == null) {
+                        this.desc = qs.getLiteral("desc").getString();
+                    }
+                });
+                log.info("desc: " + this.desc);
             }
 
         }
